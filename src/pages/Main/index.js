fec453088036a3d9f766b42b5b09439df2bb14ee
Jefferson <jeffersonbraster@gmail.com>
2021-07-31 21:48:11 -0300

    estilizando a home

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import { FaGithub, FaPlus } from "react-icons/fa";
 import * as S from "./styles";
 
 export default function Main() {
-  return <S.Title>main</S.Title>;
+  return (
+    <S.Container>
+      <h1>
+        <FaGithub size={25} /> Meus favoritos
+      </h1>
+
+      <S.Form onSubmit={() => {}}>
+        <input type="text" placeholder="Add repo" />
+
+        <S.SubmitButton>
+          <FaPlus size={14} color="#EAEAEA" />
+        </S.SubmitButton>
+      </S.Form>
+    </S.Container>
+  );
 }
