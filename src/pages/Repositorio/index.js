7578426c42ef4f9044e80ddc3cc0f88c7472655c
Jefferson <jeffersonbraster@gmail.com>
2021-08-01 21:35:20 -0300

    carregando os detalhes

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../../services/api";
+import * as S from "./styles";
 
 export default function Repositorio({ match }) {
-  return <h1>{match.params.repository}</h1>;
+  const [repo, setRepo] = useState({});
+  const [issues, setIssues] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadRepo() {
+      const nomeRepo = decodeURIComponent(match.params.repository);
+
+      const [repoData, issuesData] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),
+        api.get(`/repos/${nomeRepo}/issues`, {
+          params: {
+            state: "open",
+            per_page: 5,
+          },
+        }),
+      ]);
+
+      setRepo(repoData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    }
+
+    loadRepo();
+  }, [match.params.repository]);
+
+  return <S.Container></S.Container>;
 }
